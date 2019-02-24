c055d101e6c2a89178dee52ecd8ea727e0ed90ce
studioshin <shin@studioshin.com>
2019-02-24 16:17:38 +0900

    待機画面での動画広告再生

--- mpe/mpe/02_Waiting/WaitingViewController.swift
+++ mpe/mpe/02_Waiting/WaitingViewController.swift
@@ -8,8 +8,30 @@
 
 import UIKit
 
-class WaitingViewController: BaseViewController {
+class WaitingViewController: BaseViewController, ADViewVideoDelegate {
 
+	//MARK: ADViewVideoDelegate
+	func adViewDidPlayVideo(_ adView: ADView, incentive: Bool) {
+		
+		if incentive {
+			//ポイント
+			let pp = UserDefaults.standard.integer(forKey: kPPPoint) + 1
+			self.purchaseButton.setTitle("\(pp)", for: .normal)
+			UserDefaults.standard.set(pp, forKey: kPPPoint)
+		}
+	}
+	func adViewLoadVideo(_ adView: ADView, canLoad: Bool) {
+		
+	}
+	func adViewDidCloseVideo(_ adView: ADView, incentive: Bool) {
+		
+		SoundManager.shared.pauseBGM(false)
+		if incentive {
+			
+		}
+	}
+	
+	
 	class func waitingViewController() -> WaitingViewController {
 		
 		let storyboard = UIStoryboard(name: "WaitingViewController", bundle: nil)
@@ -82,6 +104,21 @@ class WaitingViewController: BaseViewController {
 		let settingView = SettingViewController.settingViewController()
 		settingView.present(self) { 
 			
+		}
+	}
+	
+	
+	@IBOutlet weak var videoButton: UIButton!
+	@IBAction func videoButtonAction(_ sender: Any) {
+		
+		SoundManager.shared.startSE(type: .seSelect)	//SE再生
+		if adVideoReward.isCanPlayVideo {
+			adVideoReward.playVideo()
+			SoundManager.shared.pauseBGM(true)
+		} else {
+			let alert = UIAlertController(title: nil, message: "動画を再生できませんでした！！", preferredStyle: .alert)
+			alert.addAction(UIAlertAction(title: "閉じる", style: .default, handler: nil))
+			self.present(alert, animated: true, completion: nil)
 		}
 	}
 	
